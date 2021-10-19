b6f50d4bda6494522e45353366500c1a58dd746e
CriCri1311 <cristina110495@hotmail.com>
2021-10-19 23:09:10 +0200

    Incluir

---
+++ Crisis/Entornos/P.4Repositorio/Ejercicio7Valores.java
@@ -0,0 +1,15 @@
+package OperacionesyValores;
+
+import java.util.Scanner;
+
+public class Ejercicio7Valores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int minutos = sc.nextInt();//Ejemplo 1000
+        int horas = minutos / 60;
+        int resto = minutos % 60; //puedes poner en vez de int minutos = minutos % 60;
+        System.out.println("En " + minutos + " minutos hay " + horas + " horas y " + resto + " minutos");
+    }
+
+
+}
